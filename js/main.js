a38e11d82b1321967a2c227088ac687b326871bc
Corrie <corriebotha64@gmail.com>
2022-06-21 17:10:09 +0200

    Compiled ts file

--- js/main.js
+++ js/main.js
@@ -7,29 +7,27 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 const scrollLinks = document.querySelectorAll(".scroll-links");
 navToggle.addEventListener("click", () => {
-    if (links.classList.contains("links-toggle")) {
-        links.classList.remove("links-toggle");
-    }
-    else {
-        links.classList.add("links-toggle");
-    }
+  if (links.classList.contains("links-toggle")) {
+    links.classList.remove("links-toggle");
+  } else {
+    links.classList.add("links-toggle");
+  }
 });
 links.addEventListener("click", () => {
-    if (links.classList.contains("links-toggle")) {
-        links.classList.remove("links-toggle");
-    }
+  if (links.classList.contains("links-toggle")) {
+    links.classList.remove("links-toggle");
+  }
 });
 //  Fixed navbar
 const nav = document.querySelector(".navbar");
 const projectsSection = document.querySelector(".projects");
 let projectSectionHeight = projectsSection.offsetTop - 500;
 const fixednav = () => {
-    if (window.scrollY > projectSectionHeight) {
-        nav.classList.add("fixed");
-    }
-    else {
-        nav.classList.remove("fixed");
-    }
+  if (window.scrollY > projectSectionHeight) {
+    nav.classList.add("fixed");
+  } else {
+    nav.classList.remove("fixed");
+  }
 };
 window.addEventListener("scroll", fixednav);
 // Slider
@@ -37,32 +35,30 @@ const slides = document.querySelectorAll(".slide");
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
 slides.forEach(function (slide, index) {
-    slide.style.left = `${index * 100}%`;
+  slide.style.left = `${index * 100}%`;
 });
 let counter = 0;
 nextBtn.addEventListener("click", function () {
-    counter++;
-    carousel();
+  counter++;
+  carousel();
 });
 prevBtn.addEventListener("click", function () {
-    counter--;
-    carousel();
+  counter--;
+  carousel();
 });
 function carousel() {
-    if (counter < slides.length - 1) {
-        nextBtn.style.display = "block";
-    }
-    else {
-        nextBtn.style.display = "none";
-    }
-    if (counter > 0) {
-        prevBtn.style.display = "block";
-    }
-    else {
-        prevBtn.style.display = "none";
-    }
-    slides.forEach(function (slide) {
-        slide.style.transform = `translateX(-${counter * 100}%)`;
-    });
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = "block";
+  } else {
+    nextBtn.style.display = "none";
+  }
+  if (counter > 0) {
+    prevBtn.style.display = "block";
+  } else {
+    prevBtn.style.display = "none";
+  }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
 }
 prevBtn.style.display = "none";
