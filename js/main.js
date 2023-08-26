1af211ecaa9f74f28255e22e1ffd3e2705dd1489
Corrie <corriebotha64@gmail.com>
2023-08-26 18:51:06 +0200

    Added counters for projects and lines of code written also added javascript funtion to increment the numbers automatically by 1

--- js/main.js
+++ js/main.js
@@ -6,3 +6,38 @@ btn.addEventListener("click", () => {
   nav.classList.toggle("flex");
   nav.classList.toggle("hidden");
 });
+
+/**
+ * function to increment the counter
+ *
+ * Get elements form html page
+ * create counter
+ * check if elment value is the same as the counter
+ * if so increment the element value with 1
+ */
+
+let displayValue = document.querySelectorAll("#num");
+let projectsInterval = 19;
+let linsOfCodeInterval = 1000;
+function increaseNumber(delay) {
+  displayValue.forEach((displayNum) => {
+    let startingNumber = 0;
+    let endingvalue = parseInt(displayNum.getAttribute("data-val"));
+    let duration = Math.floor(projectsInterval / endingvalue);
+    let durationTwo = Math.floor(linsOfCodeInterval / endingvalue);
+
+    let counter = setInterval(
+      () => {
+        startingNumber += 1;
+        displayNum.textContent = startingNumber;
+        if (startingNumber == endingvalue) {
+          clearInterval(counter);
+        }
+      },
+      duration,
+      durationTwo
+    );
+  });
+}
+
+increaseNumber();
